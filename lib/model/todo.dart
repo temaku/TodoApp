e5a0a7433903867183eebd41341d3f3824efcb22
Temesgen Talo <talotemesgen@gmail.com>
2021-03-01 16:52:15 +0300

    first model finished

---
+++ lib/model/todo.dart
@@ -0,0 +1,34 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+
+@immutable
+class  Todo extends Equatable {
+  Todo(
+      {this.id,
+        @required this.title,
+        @required this.description,
+        @required this.dateCreated,
+        @required this.done});
+
+  final String id;
+  final String title;
+  final String description;
+  final String dateCreated;
+  final String done;
+
+  @override
+  List<Object> get props => [id, title, description, dateCreated,done];
+
+  factory Todo.fromJson(Map<String, dynamic> json) {
+    return Course(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+      dateCreated: json['dateCreated'],
+      done: json['done'],
+    );
+  }
+
+  @override
+  String toString() => 'Course { id: $id, title: $title, description: $description,datereated: $dateCreated}';
+}
